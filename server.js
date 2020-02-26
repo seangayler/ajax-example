46b75464ca940303c98784ada0ffc2fdefc69748
Sean Gayler <seangayler@Seans-MacBook-Pro.local>
2020-02-26 23:32:57 +1100

    Setup basic ajax example.

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/shit', (req, res) => {
+  res.send('I am not shit!');
+})
+
+app.listen(3000);
